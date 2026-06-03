1ee6ae04f3d15188a8a1f657701734dbe2d8a665
Victor Vicente <vaimacana@est.ups.edu.ec>
2026-06-02 23:03:54 -0500

    Mejorar navegación ocultando encabezados

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,9 +14,14 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="register" />
+        <Stack.Screen name="dashboard" />
+        <Stack.Screen name="add-income" />
+        <Stack.Screen name="add-expense" />
+        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
